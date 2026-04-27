8094efa926757b4b6dff7e6b62c3f84342cdade6
Max Klinchik <max.klinchik@gmail.com>
2026-04-27 12:09:04 -0400

    Add x-user-id and x-user-type to CORS allowedHeaders

--- api/server.js
+++ api/server.js
@@ -59,7 +59,7 @@ app.use(cors({
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-user-id', 'x-user-type']
 }));
 
 // Handle preflight requests
